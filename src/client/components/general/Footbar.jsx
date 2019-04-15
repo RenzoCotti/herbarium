672a7f7cd8fb0090f4314a194a7415daad78b774
Renzo Cotti <fattolo170@gmail.com>
2019-04-15 16:54:02 +0200

    add fair use disclaimer

--- src/client/components/general/Footbar.jsx
+++ src/client/components/general/Footbar.jsx
@@ -2,7 +2,11 @@ import React from "react";
 
 const Footbar = props => {
   let today = new Date().getFullYear();
-  return <div className="foot sub-title">&copy; {today} Renzo Cotti</div>;
+  return (
+    <div className="foot">
+      <div className="sub-title">&copy; {today} Renzo Cotti</div>
+      <div className="foot-note">All images used under fair use.</div>
+    </div>);
 };
 
 export default Footbar;
